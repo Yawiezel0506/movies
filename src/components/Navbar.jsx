53f2f5cf90f9d54713ca26d793c62be0adc768bb
Yawiezel0506 <117453207+Yawiezel0506@users.noreply.github.com>
2023-04-20 11:04:00 +0300

    video

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,9 @@ import { onAuthStateChanged, signOut } from "firebase/auth";
 import { firebaseAuth } from "../utils/firebase-config";
 
 export default function Navbar({ isScrolled }) {
-  const navigate = useNavigate()
-  
-    const links = [
+  const navigate = useNavigate();
+
+  const links = [
     { name: "home", link: "/" },
     { name: "TV shows", link: "/tv" },
     { name: "Movies", link: "/movies" },
@@ -31,7 +31,7 @@ export default function Navbar({ isScrolled }) {
             <img src={logo} alt="logo" />
           </div>
           <ul className="links flex">
-            {links.map(({name, link}) => {
+            {links.map(({ name, link }) => {
               return (
                 <li key={name}>
                   <Link to={link}>{name}</Link>
@@ -44,7 +44,9 @@ export default function Navbar({ isScrolled }) {
           <div className={`search ${showSearch && "show-search"}`}>
             <button
               onFocus={() => setShowSearch(true)}
-              onBlur={() => !inputHover && setShowSearch(false)}
+              onBlur={() => {
+                if (!inputHover) setShowSearch(false);
+              }}
             >
               <FaSearch />
             </button>
@@ -59,8 +61,8 @@ export default function Navbar({ isScrolled }) {
               }}
             />
           </div>
-          <button onClick={()=> signOut(firebaseAuth)}>
-            <FaPowerOff/>
+          <button onClick={() => signOut(firebaseAuth)}>
+            <FaPowerOff />
           </button>
         </div>
       </nav>
@@ -69,89 +71,89 @@ export default function Navbar({ isScrolled }) {
 }
 
 const Container = styled.div`
-    .scrolled {
-        background: black;
+  .scrolled {
+    background: black;
+  }
+  nav {
+    position: sticky;
+    top: 0;
+    height: 6.5rem;
+    width: 100%;
+    justify-content: space-between;
+    position: fixed;
+    z-index: 2;
+    padding: 0 4rem;
+    align-items: center;
+    transition: 0.3s ease-in-out;
+  }
+  .left {
+    gap: 2rem;
+    .brand {
+      img {
+        height: 4rem;
+      }
     }
-    nav {
-        position: sticky;
-        top: 0;
-        height: 6.5rem;
-        width: 100%;
-        justify-content: space-between;
-        position: fixed;
-        z-index: 2;
-        padding: 0 4rem;
-        align-items: center;
-        transition: 0.3s ease-in-out;
+    .links {
+      list-style-type: none;
+      gap: 2rem;
+      li {
+        a {
+          color: white;
+          text-decoration: none;
+        }
+      }
     }
-    .left {
-        gap: 2rem;
-        .brand{
-            img{
-                height: 4rem;
-            }
-        }
-        .links {
-            list-style-type: none;
-            gap: 2rem;
-            li {
-                a {
-                    color: white;
-                    text-decoration: none;
-                }
-            } 
-        }
+  }
+  .right {
+    gap: 1rem;
+    button {
+      background: transparent;
+      border: none;
+      cursor: pointer;
+      &:focus {
+        outline: none;
+      }
+      svg {
+        color: #f34242;
+        font-size: 1.2rem;
+      }
     }
-    .right {
-        gap: 1rem;
-        button {
-            background: transparent;
-            border: none;
-            cursor: pointer;
-            &:focus {
-                outline: none;
-            }
-            svg {
-                color: #f34242;
-                font-size: 1.2rem;
-            }
+    .search {
+      display: flex;
+      gap: 0.4rem;
+      align-items: center;
+      justify-content: center;
+      padding: 0.2rem;
+      padding-left: 0.5rem;
+      button {
+        background: transparent;
+        svg {
+          color: white;
         }
-        .search {
-            display: flex;
-            gap: 0.4rem;
-            align-items: center;
-            justify-content: center;
-            padding: 0.2rem;
-            padding-left: 0.5rem;
-            button {
-                background: transparent;
-                svg {
-                    color: white;
-                }
-            }
-        }
-        input {
-            width: 0;
-            opacity: 0;
-            visibility: hidden;
-            transition: 0.3s ease-in-out;
-            background: transparent;
-            border: none;
-            color: white;
-            &:focus {
-                outline: none;
-            }
-        }
+      }
     }
-    .show-search {
-        border: 1px solid white;
-        border-radius: 0.3rem;
-        background: rgba(0,0,0,0.6);
-        input {
-            width: 100%;
-            opacity: 1;
-            visibility: visible;
-            padding: 0.3rem;
-        }
+    input {
+      width: 0;
+      opacity: 0;
+      visibility: hidden;
+      transition: 0.3s ease-in-out;
+      background: transparent;
+      border: none;
+      color: white;
+      &:focus {
+        outline: none;
+      }
     }
+  }
+  .show-search {
+    border: 1px solid white;
+    border-radius: 0.3rem;
+    background: rgba(0, 0, 0, 0.6);
+    input {
+      width: 100%;
+      opacity: 1;
+      visibility: visible;
+      padding: 0.3rem;
+    }
+  }
 `;
---
+++ src/pages/Player.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+import {BsArrowLeft} from 'react-icons/bs'
+import video from '../assets/'
+
+export default function Player() {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  
+`
